1c2dbefede0670f91d457eb5e067a8811cfd3454
Michael Riley <michaelriley87@gmail.com>
2026-07-18 16:43:29 +1000

    Initial Spring Boot backend setup

---
+++ backend/src/main/java/com/michaelriley87/liminal_chat/LiminalChatApplication.java
@@ -0,0 +1,13 @@
+package com.michaelriley87.liminal_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiminalChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiminalChatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/michaelriley87/liminal_chat/LiminalChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelriley87.liminal_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiminalChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
